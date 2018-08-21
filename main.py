947646d581507263aa43e5186e19c76462fa6754
Zichao <33247399+666zcli@users.noreply.github.com>
2018-08-21 11:31:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -159,6 +159,7 @@ def main():
         model_config = dict(model_config, **literal_eval(args.model_config))
 
     model = model(**model_config)
+    model = torch.nn.DataParallel(model, device_ids=[0, 1])
     logging.info("created model with configuration: %s", model_config)
 
     # optionally resume from a checkpoint
