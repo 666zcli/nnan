8af8b7c76fb45687bf61ee7d1639552d17cd9b28
Zichao <33247399+666zcli@users.noreply.github.com>
2018-08-25 13:25:26 -0400

    Update resnet_nan.py

--- models/resnet_nan.py
+++ models/resnet_nan.py
@@ -5,7 +5,7 @@ import nnan
 
 __all__ = ['resnet_nan', 'resnet18_nan', 'resnet34_nan', 'resnet50', 'resnet101', 'resnet152']
 
-snn = nnan.NNaNUnit(dims=[50,50])
+snn = nnan.NNaNUnit(dims=[30,30])
 
 
 def conv3x3(in_planes, out_planes, stride=1):
