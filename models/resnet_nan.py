aec9ed112033d955278d31f6cba88a77778f3275
Zichao <33247399+666zcli@users.noreply.github.com>
2018-08-24 23:31:25 -0400

    Update resnet_nan.py

--- models/resnet_nan.py
+++ models/resnet_nan.py
@@ -188,7 +188,7 @@ class ResNet_cifar10(ResNet):
                                bias=False)
         self.bn1 = nn.BatchNorm2d(16)
         self.relu = nn.ReLU(inplace=True)
-        self.snn = nnan.NNaNUnit(dims = [5,5,5,5])
+        self.snn = nnan.NNaNUnit(dims = [5,5,5])
         self.maxpool = lambda x: x
         self.layer1 = self._make_layer(block, 16, n)
         self.layer2 = self._make_layer(block, 32, n, stride=2)
