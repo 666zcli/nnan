146b61cfb81873362d51a099d37b5d0fe482e947
Zichao <33247399+666zcli@users.noreply.github.com>
2018-08-27 12:07:06 -0400

    Update nnan_dense.py

--- models/nnan_dense.py
+++ models/nnan_dense.py
@@ -23,10 +23,10 @@ class NNaNUnit(nn.Module):
         outputs = inputs.view(torch.numel(inputs), 1)
         for module in self._modules.values():
             temp = outputs
-            print temp.size()
+            #print temp.size()
             #print temp
             outputs = module(outputs)
-            print outputs.size()
+            #print outputs.size()
             outputs = temp + outputs
             
         # reshape back to the original shape
