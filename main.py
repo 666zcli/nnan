6a007a1000a1ee72c57c390e0e3fa247baa79ab1
Zichao <33247399+666zcli@users.noreply.github.com>
2018-09-06 11:39:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -560,35 +560,7 @@ def adjust_learning_rate(optimizer, epoch):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 	
-
-def genOrthgonal(dim):
-    a = torch.zeros((dim, dim)).normal_(0, 1)
-    q, r = torch.qr(a)
-    d = torch.diag(r, 0).sign()
-    diag_size = d.size(0)
-    d_exp = d.view(1, diag_size).expand(diag_size, diag_size)
-    q.mul_(d_exp)
-    return q
-
-
-def makeDeltaOrthogonal(weights):
-    rows = weights.size(0)
-    cols = weights.size(1)
-    if rows > cols:
-        print("In_filters should not be greater than out_filters.")
-    weights.data.fill_(0)
-    dim = max(rows, cols)
-    q = genOrthgonal(dim)
-    mid1 = weights.size(2) // 2
-    mid2 = weights.size(3) // 2
-    weights[:, :, mid1, mid2] = q[:weights.size(0), :weights.size(1)]
-
-def weights_init(net):
-    for m in net.modules():
-        makeDeltaOrthogonal(m.weight)
-        
-
-
+      
 
 if __name__ == '__main__':
     starttime = datetime.now()
